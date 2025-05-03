18c08cb0bd5ada6f3c3e018f1757b4220af2ad95
Antony <adurancastillo96@gmail.com>
2025-05-03 02:07:53 +0200

    ejercicio 1 done

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,9 +1,9 @@
-import { promises } from 'fs';
+import fs from 'fs';
 import { join } from 'path';
 
 // Read the file
 const filePath = join(process.cwd(), 'ejercicio-1/hacienda.json');
-const data = await promises.readFile(filePath, 'utf-8');
+const data = await fs.promises.readFile(filePath, 'utf-8');
 const fileContent = JSON.parse(data);
 
 // Print it to the console
@@ -16,18 +16,31 @@ fileContent.forEach(person => {
     console.log(person.nombre);
 });
 
-// found DNI 
-let dni = "76543210D";
-const person = fileContent.find(person => person.dni === dni);
+// found DNI: '76543210M','98765432K','32109876H' 
 
-console.log('===Iteration N3===');
+async function foundDNI(dnis) {
+for(const dni of dnis){
+const person = fileContent.find(person => person.dni === dni);
 if (person) {
-    console.log(`Name: ${person.nombre}`);
     if (person.notificacionesPendientes) {
-        console.log('There are messages from AEAT.');
+        const message = `There are messages from AEAT to ${person.nombre} with ID: ${person.dni}`;
+        const filePath = join(process.cwd(), 'ejercicio-1/notificaciones.txt');
+        await fs.promises.appendFile(filePath, message + '\r\n','utf-8');
+        console.log(message);
     } else {
-        console.log('no messages.');
+        console.log(`no messages to ${person.nombre} with ID: ${person.dni}.`);
     }
 } else {
     console.log(`No found anybody with ID: ${dni}`);
 }
+}
+}
+
+console.log('===Iteration N3 & N4===');
+const dnis = process.argv.slice(2);
+if (dnis.length === 0) {
+    console.error('Please provide at least one DNI as a command-line argument.');
+    process.exit(1);
+  }
+foundDNI(dnis);
+// 
